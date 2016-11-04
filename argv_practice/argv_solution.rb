ea88885cfa6b6e76c12439ad142b421599ab0858
Joy <joymccaffreychicago@gmail.com>
2016-11-03 19:01:37 -0500

    Complete assignment.

--- argv_practice/argv_solution.rb
+++ argv_practice/argv_solution.rb
@@ -2,4 +2,15 @@
 # Your Solution
 
 # start with this, run from terminal to see what you get.
-p ARGV
+# p ARGV
+
+# stewart, james, sue, billy
+# looks at length of stewart and compares it to james, james is shorter, compares is to sue is shorter, billy is longer, throw sue into names array
+# then just compares stewart, james, billy
+
+new_argv = ARGV
+
+new_argv.sort_by!{|name| name.length}
+  
+puts new_argv
+
